bcdefc45955143c8ec2edbb6d11a9d436ab62557
Aldi Ageng Tri Seftian <42142374+AldiAgeng@users.noreply.github.com>
2020-04-22 19:45:36 +0700

    membuat detail di tugas pertemuan10

--- kuliah/pertemuan10/functions.php
+++ kuliah/pertemuan10/functions.php
@@ -9,6 +9,10 @@ function query($query){
 
   $result = mysqli_query($conn,$query);
 
+  if(mysqli_num_rows($result) == 1){
+    return mysqli_fetch_assoc($result);
+  }
+
   $rows=[];
   while($row = mysqli_fetch_assoc($result)){
     $rows[] = $row;
