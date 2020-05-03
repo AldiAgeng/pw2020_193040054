83625eb36b4d8c8d8d0c796be8f0e70685e8717a
Aldi Ageng Tri Seftian <42142374+AldiAgeng@users.noreply.github.com>
2020-05-03 11:55:46 +0700

    memperindah tampilan login latihan7a,b,c

--- praktikum/P7_PW_193040054/latihan7b/php/login.php
+++ praktikum/P7_PW_193040054/latihan7b/php/login.php
@@ -29,38 +29,171 @@ if(isset($_POST['submit'])){
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
+  <meta charset="utf-8">
+  <title></title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      text-decoration: none;
+      font-family: montserrat;
+      box-sizing: border-box;
+    }
+
+    body {
+      min-height: 100vh;
+      background-image: linear-gradient(-90deg,  gray,rgb(130, 14, 208));
+    }
+
+    .login-form {
+      width: 360px;
+      background: #f1f1f1;
+      height: 580px;
+      padding: 80px 40px;
+      border-radius: 10px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    .login-form h1 {
+      text-align: center;
+      margin-bottom: 60px;
+    }
+
+    .txtb {
+      border-bottom: 2px solid #adadad;
+      position: relative;
+      margin: 30px 0;
+    }
+
+    .txtb input {
+      font-size: 15px;
+      color: #333;
+      border: none;
+      width: 100%;
+      outline: none;
+      background: none;
+      padding: 0 5px;
+      height: 40px;
+    }
+
+    .txtb span::before {
+      content: attr(data-placeholder);
+      position: absolute;
+      top: 50%;
+      left: 5px;
+      color: #adadad;
+      transform: translateY(-50%);
+      z-index: -1;
+      transition: .5s;
+    }
+
+    .txtb span::after {
+      content: '';
+      position: absolute;
+      width: 0%;
+      height: 2px;
+      transition: .5s;
+    }
+
+    .focus+span::before {
+      top: -5px;
+    }
+
+    .focus+span::after {
+      width: 100%;
+    }
+
+    .logbtn {
+      display: block;
+      border-radius: 120px;
+      width: 100%;
+      height: 50px;
+      border: none;
+      background: linear-gradient(120deg, gray, rgb(130, 14, 208), gray);
+      background-size: 200%;
+      color: #fff;
+      outline: none;
+      cursor: pointer;
+      transition: .5s;
+    }
+
+    .logbtn:hover {
+      background-position: right;
+    }
+
+    .bottom-text {
+      margin-top: 60px;
+      text-align: center;
+      font-size: 13px;
+    }
+    .remember{
+      margin-bottom: 20px;
+    }
+    hr{
+      position: absolute;
+      top: 130px;
+      left : 70px;
+      border-color: rgb(130, 14, 208);
+      width: 220px;
+      border-width: 1px;
+    }
+  </style>
 </head>
+
 <body>
-  <form action="" method="post">
-    <?php if(isset($error)) :  ?>
-    <p>Username password salah</p>
-    <?php endif ?>
-    <table>
-      <tr>
-        <td><label for="username">Username</label></td>
-        <td>:</td>
-        <td><input type="text" name="username"></td>
-      </tr>
-      <tr>
-        <td><label for="password">Password</label></td>
-        <td>:</td>
-        <td><input type="password" name="password"></td>
-      </tr>
-    </table>
+
+  <form action="" method="post" class="login-form">
+    <?php if(isset($error)) : 
+        echo "<script>
+              alert('Username atau Password salah');
+        </script>";
+    endif ?>
+    <h1>Login</h1>
+    <hr class="my-4">
+
+    <div class="txtb">
+      <input type="text" name="username">
+      <span data-placeholder="Username"></span>
+    </div>
+
+    <div class="txtb">
+      <input type="password" name="password">
+      <span data-placeholder="Password"></span>
+    </div>
+
     <div class="remember">
-      <input type="checkbox" name="remember">
+      <input type="checkbox" name="remember" id="remember">
       <label for="remember">Remember Me</label>
     </div>
-    <button type="submit" name="submit">Login</button>
-    <div class="registrasi">
-      <p>belum punya akun? registrasi <a href="registrasi.php">Disini</a></p>
+
+    <input type="submit" name="submit" class="logbtn" value="Login">
+
+    <div class="bottom-text">
+      Don't have account? <a href="registrasi.php">Sign up</a>
     </div>
+
   </form>
+
+  <script type="text/javascript">
+    $(".txtb input").on("focus", function () {
+      $(this).addClass("focus");
+    });
+
+    $(".txtb input").on("blur", function () {
+      if ($(this).val() == "")
+        $(this).removeClass("focus");
+    });
+  </script>
+
+
 </body>
+
 </html>
